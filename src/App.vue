7bab63eca06484c747500b7b9903ed920b650cc3
Andanteq <Kochka2244@gmail.com>
2024-11-20 09:00:04 +0400

    Создал блок search

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,40 @@
 <template>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width" intial-scale="1.0">
-    <title>Онлайн биржа для студентов</title>
-  </head>
-  <body>
-    <div class="header-top">
-      <div class="header-top-menu">
-        <ul class="header-top-menu_list">
-          <li class="header-top-menu_el">Добавить объявление</li>
-          <li class="header-top-menu_el">Создать резюме</li>
-          <li class="header-top-menu_el">Войти</li>
-        </ul>
+  <div class="header-top">
+    <div class="header-top-menu">
+      <ul class="header-top-menu_list">
+        <li class="header-top-menu_el">Добавить объявление</li>
+        <li class="header-top-menu_el">Создать резюме</li>
+        <li class="header-top-menu_el">Войти</li>
+      </ul>
+    </div>
+    <h1>ОНЛАЙН <br> БИРЖА ДЛЯ <br> СТУДЕНТОВ</h1>
+  </div>
+  <div class="selection">
+      <div class="selection-text">ПОДБОРКА ВАКАНСИЙ ДЛЯ ВАС</div>
+      <div class="selection-blocks">
+        <div class="selection-blocks_el">
+          <div class="selection-blocks_text">РАЗРАБОТЧИК</div>
+        </div>
+        <div class="selection-blocks_el">
+          <div class="selection-blocks_text">UX/UI ДИЗАЙНЕР</div>
+        </div>
+        <div class="selection-blocks_el">
+          <div class="selection-blocks_text">ПРОГРАММИСТ</div>
+        </div>
       </div>
-      <h1>ОНЛАЙН <br> БИРЖА ДЛЯ <br> СТУДЕНТОВ</h1>
-    </div>
-    <div class="selection">
-        <div class="selection-text">ПОДБОРКА ВАКАНСИЙ ДЛЯ ВАС</div>
-        <div class="selection-blocks">
-          <div class="selection-blocks_el">
-            <div class="selection-blocks_text">РАЗРАБОТЧИК</div>
-          </div>
-          <div class="selection-blocks_el">
-            <div class="selection-blocks_text">UX/UI ДИЗАЙНЕР</div>
-          </div>
-          <div class="selection-blocks_el">
-            <div class="selection-blocks_text">ПРОГРАММИСТ</div>
-          </div>
-        </div>
-        <div>        
-          <img class="left-arrow" src="./arrow_left.png" @click="LeftScroll(selection-blocks_el)">
-          <img class="right-arrow" src="./arrow_right.png" @click="RightScroll(selection-blocks_el)">
-        </div>
-    </div>
+      <div>        
+        <img class="left-arrow" src="./arrow_left.png" @click="LeftScroll(selection-blocks_el)">
+        <img class="right-arrow" src="./arrow_right.png" @click="RightScroll(selection-blocks_el)">
+      </div>
+  </div>
+  <div class="search">
+    <div class="search-text">ПОИСК ВАКАНСИЙ</div>
+  </div>
   <!-- <div class="shapka">
     <button class="profile" type="button" @mousedown="methods">
       <img src="./icon.png"/>
     </button>
   </div> -->
-  </body>
 </template>
 
 <style scoped src="./style.css"/>
